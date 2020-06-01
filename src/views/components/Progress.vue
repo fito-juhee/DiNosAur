48a684d7bc12a13ca2336abcbdb7fee0802e9d18
Juhee Baek <juhee.baek@fitogether.com>
2020-06-01 20:48:51 +0900

    progress bar color props 넘기기

--- src/views/components/Progress.vue
+++ src/views/components/Progress.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <h1 class="text__title">Progress indicators</h1>
+    <h1 class="title">Progress indicators</h1>
+    <d-progress-linear color="red">
+    </d-progress-linear>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+   data() {
+    return {
+      color: "red"
+    };
+  }
+};
 </script>
 
 <style scoped>
-.text__title {
-  font-size: 48px;
-  margin: 5px 0;
-}
-.text__subtitle {
-  font-size: 14px;
-  margin: 10px 0 30px 0;
-}
 </style>
