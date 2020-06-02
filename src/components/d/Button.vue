7f36902795a509702e245eb7c8c88d77d98c904f
Cheol_Kim <cheol.kim@fitogether.com>
2020-06-02 14:00:29 +0900

    Make Button template

---
+++ src/components/d/Button.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row align-center justify-center">
+        <button type="button" class="white--text d-btn theme--light elevation-2 d-size__default deep-purple accent-4">
+            <span class="v-btn__content">
+                <span>Click Me</span>
+            </span>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style>
+
+.elevation-2 {
+    box-shadow: 0 3px 1px -2px rgba(0,0,0,.2),0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12)!important;
+}
+.white--text {
+    color: #fff!important;
+    caret-color: #fff!important;
+}
+.deep-purple.accent-4 {
+    background-color: #6200ea!important;
+    border-color: #6200ea!important;
+}
+</style>
---
+++ src/views/components/Button.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <d-button></d-button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
