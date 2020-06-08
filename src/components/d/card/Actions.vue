16d6f174f3f4c06c39ed256b3beea0a718b0f6a8
Juhee Baek <juhee.baek@fitogether.com>
2020-06-08 17:30:09 +0900

    v-card 하위 컴포넌트 생성

---
+++ src/components/d/card/Actions.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="d-card__actions">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>
---
+++ src/components/d/card/Subtitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="d-card__subtitle">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>
